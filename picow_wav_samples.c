a61764eb9109633e59fa902c75cb972ee5fa5709
Kyle Shepard <kyle.shepard@limelyte.com>
2026-04-23 01:00:09 -0700

    .wav file header parsing completed

--- picow_wav_samples.c
+++ picow_wav_samples.c
@@ -14,9 +14,6 @@
 #include "pico/binary_info.h"
 #include "cartoon_laser.h"
 
-// so we can directly copy wav header to struct w/o padding issues
-#pragma pack(1)
-
 // pins for waveshare Pico Audio
 #define PICO_AUDIO_DATA_PIN 26
 #define PICO_AUDIO_CLOCK_PIN_BASE 27
@@ -27,7 +24,11 @@ bi_decl(bi_3pins_with_names(PICO_AUDIO_DATA_PIN, "I2S DIN", PICO_AUDIO_CLOCK_PIN
 
 #define SAMPLES_PER_BUFFER 256
 
-struct wav_header {
+// literally "WAVE" and "data"
+#define WAVE_LITERAL 0x45564157
+#define DATA_LITERAL 0x61746164
+
+struct __attribute__((packed)) wav_header {
     uint32_t riff;
     uint32_t file_size;
     uint32_t file_type; //should be "WAVE"
@@ -36,29 +37,12 @@ struct wav_header {
     uint16_t format; //should be 1 for Pulse Code Modulation (PCM)
     uint16_t num_channels;
     uint32_t sample_rate; // likely 44100
-    uint32_t idk; // (sample rate * bits per sample * channels) / 8
-    uint16_t idk2; // something else I'm not using
+    uint32_t byte_rate; // (sample rate * bits per sample * channels) / 8
+    uint16_t block_align; // (num_channels * bits_per_sample) / 8
     uint16_t bits_per_sample;
     uint32_t data; // should be "data", marker of the data section
     uint32_t data_size; // size of the data section
 };
-
-void parse_wav_header(struct wav_header * header, unsigned char *wav) {
-    // memcpy(wav, &header, 44);
-    header = (struct wav_header *) wav;
-
-    // validate header is supported format
-    char file_type[4];
-    sprintf(file_type, "%u", header->file_type);
-    assert(strcmp(file_type, "WAVE") == 0); // should always be "WAVE"
-    assert(header.format == 1); // we only support PCM
-    char data[4];
-    sprintf(data, "%u", header->data);
-    assert(strcmp(file_type, "data") == 0); // should always be "data"
-    // todo - validate num_channels?
-
-    // todo - validate data_size w/ wav length?
-}
 
 struct audio_buffer_pool *init_audio(struct wav_header *header) {
     
@@ -101,8 +85,12 @@ int main() {
 
     unsigned char *wav_file = Cartoon_Laser_wav;
     unsigned int wav_file_length = Cartoon_Laser_wav_len;
-    struct wav_header * header;
-    parse_wav_header(header, wav_file);
+    struct wav_header * header = (struct wav_header *) wav_file;
+
+    assert(header->file_type == 0x45564157);
+    assert(header->format == AUDIO_BUFFER_FORMAT_PCM_S16);
+    assert(header->data == DATA_LITERAL);
+
     struct audio_buffer_pool *ap = init_audio(header);
 
     uint32_t startPos = 44;
